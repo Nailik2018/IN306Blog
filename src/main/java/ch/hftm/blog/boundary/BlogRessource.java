0a62ad403bf01faceb5c49195e70e2dcb74f6258
= <kilualthaus@hotmail.com>
2023-06-22 19:20:06 +0200

    adding crud blog

--- src/main/java/ch/hftm/blog/boundary/BlogRessource.java
+++ src/main/java/ch/hftm/blog/boundary/BlogRessource.java
@@ -1,24 +1,45 @@
 package ch.hftm.blog.boundary;
 
 import ch.hftm.blog.control.BlogRepository;
+import ch.hftm.blog.control.BlogService;
 import ch.hftm.blog.entity.Blog;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-
 import java.util.List;
 
 @Path("blogs")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class BlogRessource {
 
     @Inject
-    BlogRepository blogRepository;
+    BlogService blogService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Blog> getBlogs(){
-        return blogRepository.listAll();
+        return blogService.getBlogs();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Blog getBlogById(@PathParam("id") Long id) {
+        return blogService.getBlogById(id);
+    }
+
+    @POST
+    public void addBlog(Blog blog) {
+        this.blogService.addBlog(blog);
+    }
+
+    @PUT
+    public void updateBlog(Blog blog) {
+        this.blogService.updateBlog(blog);
+    }
+
+    @DELETE
+    public void deleteBlog(Long id) {
+        this.blogService.deleteBlog(id);
     }
 }
--- src/main/java/ch/hftm/blog/control/BlogService.java
+++ src/main/java/ch/hftm/blog/control/BlogService.java
@@ -15,20 +15,36 @@ public class BlogService {
     BlogRepository blogRepository;
 
     @Inject
-    AuthorRepository authorRepository;
-
-    @Inject
     Logger logger;
 
     public List<Blog> getBlogs() {
         var blogs = blogRepository.listAll();
-        logger.info("Returning " + blogs.size() + " blogs");
+        logger.info("Alle Blogs " + blogs.size() + " blogs");
         return blogs;
+    }
+
+    public Blog getBlogById(Long id) {
+        return (Blog) blogRepository.findById(id);
     }
 
     @Transactional
     public void addBlog(Blog blog) {
         logger.info("Adding blog " + blog.getTitle());
         blogRepository.persist(blog);
+    }
+
+    @Transactional
+    public void updateBlog(Blog blog) {
+        blogRepository.persist(blog);
+        logger.info("Update Blog: " + blog.getId());
+    }
+
+    @Transactional
+    public void deleteBlog(Long id) {
+        Blog blog = (Blog) blogRepository.findById(id);
+        if (blog != null) {
+            blogRepository.delete(blog);
+            logger.info("Lösche Blog: " + id);
+        }
     }
 }
